6001bd89fd4b09c7fe054c4973d6538f0fa353a2
charles <charles.delmaire@gmail.com>
2014-01-27 22:40:32 -0500

    layout terminer + debut de la connexion avec google

--- QingPool/src/net/charlesdelmaire/qingpool/connexionActivity.java
+++ QingPool/src/net/charlesdelmaire/qingpool/connexionActivity.java
@@ -1,26 +1,54 @@
 package net.charlesdelmaire.qingpool;
 
+import com.google.android.gms.common.*;
+import com.google.android.gms.common.GooglePlayServicesClient.*;
+import com.google.android.gms.plus.PlusClient;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class connexionActivity extends Activity 
-implements OnClickListener{
+implements View.OnClickListener, ConnectionCallbacks, OnConnectionFailedListener {
+	
+	 private static final String TAG = "connexionActivity";
+	 private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
+
+	 private ProgressDialog mConnectionProgressDialog;
+	 private PlusClient mPlusClient;
+	 private ConnectionResult mConnectionResult;
+
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.connexion);
+
 		
 		View btnClick = findViewById(R.id.btnConnOk);
 	    btnClick.setOnClickListener(this);
 	    
 	    View btnClick1 = findViewById(R.id.btnConnReset);
 	    btnClick1.setOnClickListener(this);
+	    
+	    mPlusClient = new PlusClient.Builder(this, this, this)
+        .setActions("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity")
+        .setScopes(Scopes.PLUS_LOGIN)  // recommended login scope for social features
+        //.setScopes("profile")       // alternative basic login scope
+        .build();
+	    findViewById(R.id.sign_in_button).setOnClickListener(this);
+	    mConnectionProgressDialog = new ProgressDialog(this);
+        mConnectionProgressDialog.setMessage("Signing in...");
+		
+	    
+	 
 	}
 
 	@Override
@@ -49,6 +77,62 @@ implements OnClickListener{
 			
 		}
 		
+		
+		if (v.getId() == R.id.sign_in_button && !mPlusClient.isConnected()) {
+	        if (mConnectionResult == null) {
+	            mConnectionProgressDialog.show();
+	        } else {
+	            try {
+	                mConnectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+	            } catch (SendIntentException e) {
+	                // Try connecting again.
+	                mConnectionResult = null;
+	                mPlusClient.connect();
+	            }
+	        }
+	    }
+		
 	}
+
+	 @Override
+	    public void onConnectionFailed(ConnectionResult result) {
+	      if (mConnectionProgressDialog.isShowing()) {
+	        // The user clicked the sign-in button already. Start to resolve
+	        // connection errors. Wait until onConnected() to dismiss the
+	        // connection dialog.
+	        if (result.hasResolution()) {
+	          try {
+	                   result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+	           } catch (SendIntentException e) {
+	                   mPlusClient.connect();
+	           }
+	        }
+	      }
+	      // Save the result and resolve the connection failure upon a user click.
+	      mConnectionResult = result;
+	    }
+
+	  @Override
+	    public void onConnected(Bundle connectionHint) {
+	        String accountName = mPlusClient.getAccountName();
+	        Toast.makeText(this, accountName + " is connected.", Toast.LENGTH_LONG).show();
+	    }
+
+	  @Override
+	    public void onDisconnected() {
+	        Log.d(TAG, "disconnected");
+	    }
+	
+	@Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        if (requestCode == REQUEST_CODE_RESOLVE_ERR && responseCode == RESULT_OK) {
+            mConnectionResult = null;
+            mPlusClient.connect();
+        }
+    }
+	
+	
+
+	
 
 }
--- QingPool/src/net/charlesdelmaire/qingpool/listPartiActivity.java
+++ QingPool/src/net/charlesdelmaire/qingpool/listPartiActivity.java
@@ -1,17 +1,52 @@
 package net.charlesdelmaire.qingpool;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class listPartiActivity extends Activity  {
+	List<Map<String, String>> partList = new ArrayList<Map<String,String>>();
+	SimpleAdapter simpleAdpt;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listparti);
+		initList();
+		
+		ListView lv = (ListView) findViewById(R.id.listParticipant);
+		
+		simpleAdpt = new SimpleAdapter(this, partList, android.R.layout.simple_list_item_1, new String[] {"nomPart"}, new int[] {android.R.id.text1});
+		lv.setAdapter(simpleAdpt);
 		
 
+		
+		registerForContextMenu(lv);
+
+	}
+	
+	
+	private void initList() {
+		partList.add(createPart("nomPart","proPool"));
+		partList.add(createPart("nomPart","proPool1"));
+		partList.add(createPart("nomPart","proPool2"));
+		partList.add(createPart("nomPart","proPool3"));
+		partList.add(createPart("nomPart","proPool4"));
+		partList.add(createPart("nomPart","proPool5"));
+		partList.add(createPart("nomPart","proPool6"));
+		}
+	
+	private HashMap<String, String> createPart(String key, String name) {
+	    HashMap<String, String> Pool = new HashMap<String, String>();
+	    Pool.put(key, name);
+	    return Pool;
 	}
 
 }
